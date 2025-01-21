f3ad24177b476643fbdf64536a1c72be35414a40
Aadil-1245 <135581341+Aadil-1245@users.noreply.github.com>
2025-01-21 21:42:27 +0530

    Add files via upload

---
+++ Having Word Limit in Paragraph  by Slicing - JS.js
@@ -0,0 +1,4 @@
+// In this one we will be able to limit the no of characters that we type like in the twitter 
+var name = prompt("Please Enter Your Name : ");
+alert(name.slice(0,30));
+console.log(alert);
