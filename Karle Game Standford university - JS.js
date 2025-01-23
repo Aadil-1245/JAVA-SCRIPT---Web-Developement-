18ed0be57572f5b879a29050d95b9a45c84c2425
Aadil-1245 <135581341+Aadil-1245@users.noreply.github.com>
2025-01-23 11:46:06 +0530

    Add files via upload

---
+++ Karle Game Standford university - JS.js
@@ -0,0 +1,43 @@
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to 
+ * write any Karel program you want.
+ **/
+function main(){
+    //your code here
+    hello();
+    
+ }
+ function hello () {
+    
+    move();
+    turnLeft();
+    move();
+    putBeeper();
+    turnRight();
+    move();
+    turnLeft();
+    move();
+    putBeeper();
+    turnRight();
+    move();
+    turnLeft();
+    move();
+    putBeeper();
+    turnRight();
+    move();
+    turnLeft();
+    move();
+    putBeeper();
+    turnRight();
+    turnRight();
+    move();
+    move();
+    move();
+    move();
+    
+    
+    
+   
+ }
+ 
