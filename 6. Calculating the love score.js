2bbf1153f5d8b7c6f4e6aaef081b455b7d781e13
Aadil-1245 <135581341+Aadil-1245@users.noreply.github.com>
2025-01-28 22:50:04 +0530

    Add files via upload

---
+++ 6. Calculating the love score.js
@@ -0,0 +1,5 @@
+//calculating the love score 
+prompt =  ("Enter your Name :")
+prompt = ("Enter your girl name :")
+var lovescore = Math.randome() * 100 ;
+console.log("Score for this couple is :", lovescore );
