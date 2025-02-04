37bd4dd04b33c2dc727165e36edba9b106f201b2
Aadil-1245 <135581341+Aadil-1245@users.noreply.github.com>
2025-02-04 21:58:50 +0530

    Add files via upload

---
+++ Arrays Checking the Guest name in js.js
@@ -0,0 +1,9 @@
+var guestList = [ "Aadil","vijay","Ajay", "paul"];
+var guestName = prompt("What is your name ? : ");
+if (guestList.includes(guestName)){
+    alert("Welcome to the party and Thanks for coming")
+
+}
+else {
+    alert("Sorry May be Next Time ")
+}
